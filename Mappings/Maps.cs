1fa5a402c1715905d3793f93e60d76aa89582c28
fakhrulazreen <fakhrul131096@gmail.com>
2021-06-01 22:45:53 +0800

    Commit All

---
+++ Mappings/Maps.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using leave_management.Data;
+using leave_management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap(); //reversemap() either direction should be map
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap(); // map each class in view model
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+        }
+    }
+}
---
+++ Models/EmployeeVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class EmployeeVM
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string TaxId { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
